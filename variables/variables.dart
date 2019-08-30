18655c95a828fbeeb7962f51ad9c90e48daceb8b
NandanSatheesh <nandan.199776@gmail.com>
2019-08-31 00:08:58 +0530

    Variables

---
+++ variables/variables.dart
@@ -0,0 +1,19 @@
+void main() {
+  int amount1 = 100 ;
+  var amount2 = 100 ;
+
+  print("Hi $amount1 $amount2");
+
+  var name1 = "Something";
+  String name2 = "Something Else";
+
+  dynamic weakVariable = 100 ;
+
+  weakVariable = "String";
+
+  weakVariable = null;
+
+
+
+
+}
