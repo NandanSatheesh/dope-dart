a535222749f5c6441fe0888afc0253af3fc45a9a
NandanSatheesh <nandan.199776@gmail.com>
2019-08-31 00:11:49 +0530

    Strings

---
+++ basics/strings.dart
@@ -0,0 +1,12 @@
+void main() {
+
+  int age = 10;
+  var s = "This is a string \n$age";
+  var multiLine = """
+  This is a multiline string. 
+  This is a second line.
+  """;
+  print(s);
+  print(multiLine);
+
+}
