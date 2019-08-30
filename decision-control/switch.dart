37bdcb2fa0ab42111702d63695bf45c39cd1e5a5
NandanSatheesh <nandan.199776@gmail.com>
2019-08-31 00:48:42 +0530

    Switch

---
+++ decision-control/switch.dart
@@ -0,0 +1,16 @@
+void main() {
+  int number = 100;
+
+  switch (number) {
+    case 1:
+      print("One");
+      break;
+
+    case 2:
+      print("Two");
+      break;
+
+    default:
+      print("IDK");
+  }
+}
