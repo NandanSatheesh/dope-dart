6dcb025c30900416deba758557fc23d8520ae9e9
NandanSatheesh <nandan.199776@gmail.com>
2019-08-31 00:16:31 +0530

    Operators

--- basics/conversion.dart
+++ basics/conversion.dart
@@ -1,3 +1,9 @@
-void main() {
+import 'dart:core' as dt;
 
+void main() {
+  var one = dt.int.parse("123");
+  var deciaml = dt.double.parse("2234.123");
+  const INT = 123;
+  dt.print(INT);
+  dt.print(one + deciaml);
 }
